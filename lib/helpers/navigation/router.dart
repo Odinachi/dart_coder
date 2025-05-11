82d261ee65453eeec7b0985631be61948997f040
Odinachi David <odinachidavid@yahoo.com>
2025-05-11 10:32:20 +0100

    setup home

---
+++ lib/helpers/navigation/router.dart
@@ -0,0 +1,26 @@
+import 'package:dartcoder/views/course_details.dart';
+import 'package:dartcoder/views/dart_coder.dart';
+import 'package:dartcoder/views/home.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppRouter {
+  static final navKey = GlobalKey<NavigatorState>();
+
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case courseDetails:
+        return CupertinoPageRoute(builder: (_) => const CourseDetails());
+      case home:
+        return CupertinoPageRoute(builder: (_) => const HomeScreen());
+      case editor:
+        return CupertinoPageRoute(builder: (_) => const DartCompilerApp());
+
+      default:
+        return CupertinoPageRoute(builder: (_) => const HomeScreen());
+    }
+  }
+
+  static const String courseDetails = "/course_details";
+  static const String home = "/";
+  static const String editor = "/editor";
+}
